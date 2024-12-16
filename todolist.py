8b9925168c453bd74f0002738683d69d32992d1f
Vogon-P0et <paul@innvisia.com>
2024-12-16 10:22:38 -0500

    added loops and changed menu

--- todolist.py
+++ todolist.py
@@ -1,28 +1,32 @@
 #To Do List
 
 active = True
+tasks = []
+
 while active:
-    print("Welcome to your brain...Here are the things it thinks you need done...")
-    print("\nPlease select one of the following options to begin.")
-    choice = input("Where would you like to begin? Please select from options 1-4\n")
+    print("\nWelcome to your brain...Here are the things it thinks you need done...")
+    print("\nPlease select one of the following:\n")
     print("1. Add tasks")
     print("2. View tasks")
     print("3. Delete tasks")
     print("4. Quit the Application\n")
     print("You may type quit at any time to exit the application\n")
-
-    tasks = []
+    
+    choice = input("Where would you like to begin? Please select from options 1-4\n")
+    
     if choice == "1":
-        task = input("What would you like to name this task?\n")
-        if task == "quit": #I'm not sure if this is needed, terminal vs application?
+        task_name = input("\nWhat would you like to name this task?\n")
+        if task_name.lower() == "quit":
             break
         else:
-            tasks.append(task) #this isn't working to add a new task to the list
+            tasks.append(task_name)
         print(f"{tasks[-1]} has been successfully added.")     
     if choice == "2":
-        print(tasks)
+        for task in tasks:
+            print(f"{tasks}, \n")
     if choice == "3":
-        print(tasks)
+        for task in tasks:
+            print(task)
         item_to_delete = input("""Which task would you like to delete?
                                \nPlease choose the task based on the 
                                position on the list""")
